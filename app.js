7d2c24d8aa1b1811cbceaed56747e84e5932b563
Mateus Malvezzi <mmalvezzi@limaconsulting.com>
2022-10-05 22:26:38 -0300

    Fix button check and trash || adding css

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
-todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //functions
 
@@ -14,28 +15,34 @@ function addTodo(event){
     event.preventDefault();
 
     //Todo DIV
-    const todoDIV = document.createElement('div');
-    todoDIV.classList.add("todo");
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
 
     //Create LI
     const newTodo = document.createElement('li');
-    newTodo.classList.add('todo-item');
+    newTodo.classList.add("todo-item");
     newTodo.innerHTML = todoInput.value;
-    todoDIV.appendChild(newTodo);
+    todoDiv.appendChild(newTodo);
     
     //check mark button
-    const completedButton = document.createElement('button');
-    completedButton.innerHtml = '<i class="fas fa-check"></i>';
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
     completedButton.classList.add("complete-btn");
-    todoDIV.appendChild(completedButton);
+    todoDiv.appendChild(completedButton);
 
     //check trash button
-    const trashButton = document.createElement('button');
-    trashButton.innerHtml = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("trashButton");
-    todoDIV.appendChild(trashButton);
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
 
     //Append to list
-    todoList.appendChild(todoDIV);
+    todoList.appendChild(todoDiv);
+
+    //Clear todo input value
+    todoInput.value = "";
+
+
+
 
 }
