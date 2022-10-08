0ecf15da1d19ebbe86aa2b9e6b62c338b9161fc0
MateusMalvezzi <mateusmalvezzir@hotmail.com>
2022-10-08 16:34:26 -0300

    Created: Filter function to separete tasks in all, complete and uncompleted

--- app.js
+++ app.js
@@ -2,23 +2,24 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector('.filter-todo')
 
 //Event Listeners
     todoButton.addEventListener("click", addTodo);
     todoList.addEventListener("click", deleteTodo);
+    filterOption.addEventListener("click", filterTodo);
 
 //functions
-
 function addTodo(event){
 
     //prevent form from submitting
     event.preventDefault();
 
-    //Todo DIV
+    //Todo Div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
 
-    //Create LI
+    //Create li
     const newTodo = document.createElement("li");
     newTodo.classList.add("todo-item");
     newTodo.innerHTML = todoInput.value;
@@ -48,12 +49,35 @@ function deleteTodo(e) {
         const todo = item.parentElement;
         todo.remove();
     }
-
     //check mark
     if(item.classList[0] === "complete-btn"){
         const todo = item.parentElement;
         todo.classList.toggle("completed");
         //The toggle() method toggles between hide() and show() for the selected elements
-
     }
+}
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });    
+
 }
